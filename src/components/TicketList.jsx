a9082e6549a31a593304041b2c0736484b1b8bef
Sneha Gupta <96808735+Snehagupta1907@users.noreply.github.com>
2024-02-04 13:52:17 +0530

    Update TicketList.jsx

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -137,7 +137,7 @@ const TicketList = () => {
   };
 
   return (
-    <div className="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 dark:bg-black bg-gray-300 pt-6 px-9">
+    <div className="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 dark:bg-black min-h-screen bg-gray-300 pt-6 px-9">
       {orderedTickets.map((group) => (
         <div key={group.key}>
           <div className="flex items-center justify-between  md:max-w-[250px] pb-2">
