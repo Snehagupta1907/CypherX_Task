0a9c660f89f546975be8f457b98f72827f43972a
Sneha Gupta <96808735+Snehagupta1907@users.noreply.github.com>
2024-02-04 13:47:35 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import TicketList from "../components/TicketList";
 
 const Home = () => {
   return (
-    <div>
+    <div className="min-h-screen">
       <Navbar />
       <TicketList />
     </div>
